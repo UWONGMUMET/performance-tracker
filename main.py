fb8e4d5523e962d353cba5862535de885c3da640
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-04-07 23:21:41 +0700

    register user router

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI
-from routers import auth
+from routers import auth, user
 from core.config import settings
 
 app = FastAPI(debug=settings.DEBUG)
 
 app.include_router(auth.router)
+app.include_router(user.router)
 
 @app.get("/")
 async def root():
