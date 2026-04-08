56bd2a7566c644103dcb676749394401f72c8747
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-04-08 21:37:33 +0700

    feat: add role column and fix the the correct created_at default

--- alembic/env.py
+++ alembic/env.py
@@ -6,7 +6,7 @@ from sqlalchemy import pool
 from alembic import context
 
 from db.base import Base
-from models import user
+from models import user, task
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
