cd26217d079f405581974d7ba1ce2d754c0d1fa9
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-04-08 08:20:07 +0700

    feat: register dashboard router

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 from fastapi import FastAPI
-from routers import auth, user
+from routers import auth, user, dashboard
 from core.config import settings
 
 app = FastAPI(debug=settings.DEBUG)
 
 app.include_router(auth.router)
 app.include_router(user.router)
+app.include_router(dashboard.router)
 
 @app.get("/")
 async def root():
