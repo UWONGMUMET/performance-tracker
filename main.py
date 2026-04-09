7a76fabb29bbbe232745f283ce7e506f3e933e99
UWONGMUMET <firmanhasibuan14042004@gmail.com>
2026-04-09 17:53:26 +0700

    feat: register task_dashboard routers

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from routers import auth, user, dashboard, task
+from routers import auth, user, dashboard, task, task_dashboard
 from core.config import settings
 
 app = FastAPI(debug=settings.DEBUG)
@@ -8,6 +8,7 @@ app.include_router(auth.router)
 app.include_router(user.router)
 app.include_router(dashboard.router)
 app.include_router(task.router)
+app.include_router(task_dashboard.router)
 
 @app.get("/")
 async def root():
